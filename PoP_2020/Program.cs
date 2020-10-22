d373e4c3bab143dff0fcf1b3625c38ef1857ca73
Sarah ZIANE <sarahziane5@gmail.com>
2020-10-22 12:48:06 +0300

    Ajoutez des fichiers projet.

---
+++ PoP_2020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PoP_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leva ?");
+            double leva = Convert.ToInt32(Console.ReadLine());
+            double euro = leva / 1.95583;
+            Console.WriteLine(euro);
+
+            // ou
+
+            Console.WriteLine("_leva?");
+            var _leva = int.Parse(Console.ReadLine());
+            var _euro = _leva / 1.95583;
+            Console.WriteLine(_euro);
+
+            Console.ReadKey();
+        }
+    }
+}
